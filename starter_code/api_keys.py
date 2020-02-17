f6b20970117468073fcca542a72036dcca720af5
Nandi <bhavani.sathya@outlook.com>
2020-02-16 20:54:59 -0500

    remove api key from github

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "c9f4ab8476ae8840949b25ca3753e2ed"
+weather_api_key = "insert key here"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "insert key here"
